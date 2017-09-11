9070290913ad4074b273e4a112925161cd95166e
Jing Lin <linj2@rose-hulman.edu>
2017-09-10 21:30:33 -0400

    Does pycharm have the method can get the X- and Y- coordinate of center/midpoint for us?

--- src/m6_using_objects.py
+++ src/m6_using_objects.py
@@ -5,8 +5,8 @@ This module lets you practice  ** using objects **, including:
   -- accessing their DATA via INSTANCE VARIABLES
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Jing Lin.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -14,6 +14,9 @@ import rosegraphics as rg
 def main():
     """ Calls the other functions to demonstrate and/or test them. """
     # Test your functions by putting calls to them here:
+    lines()
+    circle_and_rectangle()
+    two_circles()
 
 
 def two_circles():
@@ -27,12 +30,24 @@ def two_circles():
     -- Waits for the user to press the mouse, then closes the window.
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement this function, per its doc-string above.
+    # Done: 2. Implement this function, per its doc-string above.
     #    -- ANY two rg.Circle objects that meet the criteria are fine.
     #    -- File  COLORS.txt  lists all legal color-names.
     # Put a statement in   main   to test this function
     #    (by calling this function).
     # ------------------------------------------------------------------
+    window = rg.RoseWindow(700,400,'m6_using_objects')
+
+    c1 = rg.Circle(rg.Point(150, 150), 100)
+    c2 = rg.Circle(rg.Point(250, 100), 50)
+
+    c1.fill_color = 'yellow'
+
+    c1.attach_to(window)
+    c2.attach_to(window)
+
+    window.render()
+    window.close_on_mouse_click()
 
 
 def circle_and_rectangle():
@@ -67,7 +82,7 @@ def circle_and_rectangle():
            150.0
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement this function, per its doc-string above.
+    # Done: 3. Implement this function, per its doc-string above.
     #   -- ANY objects that meet the criteria are fine.
     # Put a statement in   main   to test this function
     #    (by calling this function).
@@ -75,6 +90,35 @@ def circle_and_rectangle():
     # IMPORTANT: Use the DOT TRICK to guess the names of the relevant
     #       instance variables for outline thickness, etc.
     # ------------------------------------------------------------------
+    window = rg.RoseWindow(700, 400)
+    Cx = 150
+    Cy = 150
+    c1 = rg.Circle(rg.Point(Cx,Cy),100)
+
+    Rx1 = 100
+    Ry1 = 250
+    Rx2 = 300
+    Ry2 = 350
+    r1 = rg.Rectangle(rg.Point(Rx1, Ry1),rg.Point(Rx2,Ry2))
+
+    c1.fill_color = 'blue'
+
+    print(c1.outline_thickness)
+    print(c1.fill_color)
+    print(c1.center)
+    print(Cx)
+    print(Cy)
+
+    print(r1.outline_thickness)
+    print(r1.fill_color)
+    print(r1.get_center())
+    print((Rx2+Rx1)/2)
+    print((Ry2+Ry1)/2)
+
+    c1.attach_to(window)
+    r1.attach_to(window)
+    window.render()
+    window.close_on_mouse_click()
 
 
 def lines():
@@ -100,9 +144,27 @@ def lines():
     -- Waits for the user to press the mouse, then closes the window.
     """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # Done: 4. Implement and test this function.
     # ------------------------------------------------------------------
+    window = rg.RoseWindow()
+    x1 = 100
+    y1 = 100
+    x2 = 200
+    y2 = 200
+    L1 = rg.Line(rg.Point(100, 100),rg.Point(100, 200))
+    L2 = rg.Line(rg.Point(x1,y1),rg.Point(x2,y2))
 
+    L2.thickness=5
+
+    L1.attach_to(window)
+    L2.attach_to(window)
+
+    print(L2.get_midpoint())
+    print((x1+x2)/2)
+    print((y1+y2)/2)
+
+    window.render()
+    window.close_on_mouse_click()
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
